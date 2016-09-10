56b9a5dbbe44b0bcffe8aa20708829b288420890
cloudera <cloudera@quickstart.cloudera>
2016-09-10 05:50:08 -0700

    commit testjar1

---
+++ testjar1/src/main/java/com/my/UserFormat.java
@@ -0,0 +1,13 @@
+package com.my;
+
+import com.ddp.access.TableGenerator;
+
+
+/**
+ * Created by cloudera on 9/3/16.
+ */
+public class UserFormat implements TableGenerator{
+    public void generate() {
+        System.out.println("this is UserFormat");
+    }
+}
