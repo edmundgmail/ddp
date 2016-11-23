3f06d5fd579b5996e8076a469794d99897a1f1b1
edmund guo <edmundguo@gmail.com>
2016-11-22 21:26:28 -0500

    update

--- sb-admin-angular/app/scripts/directives/hdfsfileuploader/hdfsfileuploader.js
+++ sb-admin-angular/app/scripts/directives/hdfsfileuploader/hdfsfileuploader.js
@@ -4,15 +4,33 @@ angular.module('sbAdminApp')
         //restrict: 'E',
         //replace: true,
         scope: {
+            uploadedpath : '=',
             uploadedfiles: '=',
             multiple:'@'
-        },
+          },
         templateUrl:'scripts/directives/hdfsfileuploader/hdfsfileuploader.html',
-      controller: function($scope) {        
+      controller: function($scope, $rootScope,  $http) {        
         $scope.upload=function(element) {
-          console.log('from directive', $scope.multiple); 
-          console.log('file=', element.files[0].name);  
-        };
-      }
+            var formdata = new FormData();
+            
+            for( var i=0;  i<element.files.length; i++){
+              formdata.append(element.files[i].name, element.files[i]);  
+            }
+            
+            $http.post($rootScope.url+'/file', formdata, {
+                  withCredentials: false,
+                  transformRequest: angular.identity,
+                  headers: {'Content-Type': undefined}})
+              .success(function(response){
+                    $scope.uploadedpath = response.uploadPath;
+                    $scope.uploadedfiles=[];
+                    angular.forEach(element.files, function(value, key){$scope.uploadedfiles.push(value.name);});
+                    alert('path=' + $scope.uploadedpath);
+                    angular.forEach($scope.uploadedfiles, function(value, key){alert('filename=' + value);});
+                }).error(function(){
+                  alert('error');            
+                });
+          }; //end of function upload()
+        }//end of controler
     }
-});
+  });
