1f65f2e1d8568eab13b785831af8e4b95a1c95ff
edmund guo <edmund.guo@gmail.com>
2017-05-23 07:43:27 -0700

    update

--- React_Starter/src/views/DataExplorer/DataExplorer.js
+++ React_Starter/src/views/DataExplorer/DataExplorer.js
@@ -3,10 +3,11 @@ import {Treebeard,decorators} from 'react-treebeard';
 import { StyleRoot } from 'radium';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-
 //import data from './data';
 import styles from './styles';
 import * as filters from './filter';
+import Globals from '../Globals';
+import AddNewDataModal from '../../components/AddNewDataModal';
 
 const HELP_MSG = 'Select A Node To See Its Data Structure Here...';
 var data={name:"root"};
@@ -55,17 +56,16 @@ class DataExplorer extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            modal: false,
-            level: 'Data Source',
-            data:[]
+            level: 'datasource',
+            data:data
         };
         this.onToggle = this.onToggle.bind(this);
-        this.toggle = this.toggle.bind(this);
 
     }
 
     componentDidMount() {
-        fetch(`http://192.168.1.130:8082/hierarchy`)
+
+        fetch(Globals.urlHierarchy)
             .then(result=> {
                 if (result.status >= 400) {
                     throw new Error("Bad response from server");
@@ -73,25 +73,17 @@ class DataExplorer extends React.Component {
                 return result.json();
             }).then(r=>{
                 data = {name: 'root', children: r};
-                this.setState({data: {name: 'root', children: r}});
+                this.setState({data: data});
             });
     }
 
-    toggle() {
 
-        if(this.state.cursor && (!this.state.cursor.level  || this.state.cursor.level == 'datasource'))
-        {
-            this.setState({
-                modal: !this.state.modal
-            });
-        }
-    }
 
     onToggle(node, toggled){
         if(this.state.cursor){this.state.cursor.active = false;}
         node.active = true;
         if(!node.children && node.level!='datafield') {
-            var url = "http://192.168.1.130:8082/hierarchy?level="+node.level+"&&id="+node.id;
+            var url = Globals.urlHierarchy+"?level="+node.level+"&&id="+node.id;
             fetch(url)
                 .then(result=> {
                     if (result.status >= 400) {
@@ -113,15 +105,6 @@ class DataExplorer extends React.Component {
         this.setState({data: filtered});
     }
     render(){
-        var addnew;
-        if(this.state.cursor && this.state.cursor.level=='dataentity'){
-            addnew= <ModalHeader toggle={this.toggle}>Add New Data Entity</ModalHeader>
-        }
-            else{
-            addnew=<ModalHeader toggle={this.toggle}>Add New Data Source</ModalHeader>
-        }
-
-
             return (
             <StyleRoot>
                 <div className="row">
@@ -138,19 +121,6 @@ class DataExplorer extends React.Component {
                     </div>
 
                 </div>
-                    <div className="card-block">
-                        <Button onClick={this.toggle}>+</Button>
-                        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-                            {addnew}
-                            <ModalBody>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                            </ModalBody>
-                            <ModalFooter>
-                                <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
-                                <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-                            </ModalFooter>
-                        </Modal>
-                    </div>
                 </div>
                 <div className="row">
                 <div style={styles.component}>
