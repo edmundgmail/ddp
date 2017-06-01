a51e3ee5bb2d8f3942e47df3ace5a5d8bae4765b
edmund junjie guo <edmundguo@gmail.com>
2017-06-01 16:18:11 -0700

    update

--- React_Starter/src/views/LoadEntity/LoadEntity.js
+++ React_Starter/src/views/LoadEntity/LoadEntity.js
@@ -8,17 +8,76 @@ class LoadEntity extends Component {
         this.state = {
             level: '',
             options: [],
-            entityOptions:[],
-            dataSource:'',
-            dataEntity:'',
-            tableData:[],
+            entityOptions: [],
+            dataSource: '',
+            dataEntity: '',
+            tableData: [],
+            showNewSource: false,
+            newSourceName: ''
         };
 
-        this.changeSource=this.changeSource.bind(this);
-        this.changeEntity=this.changeEntity.bind(this);
-        this.loadEntities=this.loadEntities.bind(this);
-        this.onAddFile=this.onAddFile.bind(this);
-        this.handleSubmit=this.handleSubmit.bind(this);
+        this.changeSource = this.changeSource.bind(this);
+        this.changeEntity = this.changeEntity.bind(this);
+        this.loadEntities = this.loadEntities.bind(this);
+        this.onAddFile = this.onAddFile.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.toggleNewSource = this.toggleNewSource.bind(this);
+        this.handleDataSourceChangeEvent = this.handleDataSourceChangeEvent.bind(this);
+        this.createNewSource = this.createNewSource.bind(this);
+    }
+
+    createNewEntity(e){
+        e.preventDefault();
+        if(this.state.newEntityName===""){
+            alert("Entity name can't be empty!");
+            return;
+        }
+        else if(this.state.entityOptions.some(v=>this.state.newEntityName===v.key)){
+            alert("The name already exists, please use a different name");
+            return;
+        }
+        else{
+            this.setState({entityOptions: this.state.entityOptions.concat(<option key={this.state.newEntityName} value={this.state.newEntityName}>{this.state.newEntityName}</option>)});
+            this.toggleNewEntity(e);
+        }
+    }
+
+
+    handleDataEntityChangeEvent(e) {
+        this.setState({newEntityName:e.target.value})
+    }
+
+    toggleNewEntity(e){
+        e.preventDefault();
+        this.setState({showNewEntity: !this.state.showNewEntity, newEntityName:''})
+    }
+
+
+
+    createNewSource(e){
+        e.preventDefault();
+        if(this.state.newSourceName===""){
+            alert("Source name can't be empty!");
+            return;
+        }
+        else if(this.state.options.some(v=>this.state.newSourceName===v.key)){
+            alert("The name already exists, please use a different name");
+            return;
+        }
+        else{
+            this.setState({options: this.state.options.concat(<option key={this.state.newSourceName} value={this.state.newSourceName}>{this.state.newSourceName}</option>)});
+            this.toggleNewSource(e);
+        }
+    }
+
+
+    handleDataSourceChangeEvent(e) {
+        this.setState({newSourceName:e.target.value})
+    }
+
+    toggleNewSource(e){
+        e.preventDefault();
+        this.setState({showNewSource: !this.state.showNewSource, newSourceName:''})
     }
 
     componentDidMount() {
@@ -40,13 +99,15 @@ class LoadEntity extends Component {
     loadEntities(databaseName){
         fetch(Globals.urlHiveHierarchy+"?level=datasource&&name="+databaseName)
             .then(result=> {
-                if (result.status >= 400) {
-                    throw new Error("Bad response from server");
-                }
                 return result.json();
             }).then(res=>{
                 let r=res.result;
-            this.setState({entityOptions: r.map(s=>( <option key={s.tableName} value={s.tableName}>{s.tableName}</option>)), dataEntity:r[0].tableName});
+                if(r && r.length>0){
+                    this.setState({entityOptions: r.map(s=>( <option key={s.tableName} value={s.tableName}>{s.tableName}</option>)), dataEntity:r[0].tableName});
+                }
+                else{
+                    this.setState({entityOptions:[]});
+                }
         });
     }
 
@@ -92,6 +153,9 @@ class LoadEntity extends Component {
             this.setState({tableData:r.result})
         });
     }
+
+
+
     render(){
       //alert(this.props.params.sourceId);
       //alert(this.props.params.id);
@@ -102,10 +166,14 @@ class LoadEntity extends Component {
                 <form action="" method="post" encType="multipart/form-data" className="form-horizontal">
                     <div className="form-group row">
                         <label className="col-md-2 form-control-label" htmlFor="text-input">Select Source</label>
-                        <div className="col-md-6">
+                        <div className="col-md-4">
                             <select className="form-control" id="sources" onChange={this.changeSource}>
                                 {this.state.options}
                             </select>
+                        </div>
+                        <div className="row">
+                            <button type="submit" className="btn" onClick={this.toggleNewSource}>+</button>
+                            { this.state.showNewSource ? <div className="row"> <input type="text" placeholder="New DataSource Name"  onChange={this.handleDataSourceChangeEvent}/> <input type="submit" value="Add" onClick={this.createNewSource}/> </div> : null }
                         </div>
                     </div>
 
@@ -116,6 +184,11 @@ class LoadEntity extends Component {
                                 {this.state.entityOptions}
                             </select>
                         </div>
+                        <div className="row">
+                            <button type="submit" className="btn" onClick={this.toggleNewEntity}>+</button>
+                            { this.state.showNewEntity? <div className="row"> <input type="text" placeholder="New DataEntity Name"  onChange={this.handleDataEntityChangeEvent}/> <input type="submit" value="Add" onClick={this.createNewEntity}/> </div> : null }
+                        </div>
+
                     </div>
 
                     <div className="form-group row">
